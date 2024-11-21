c6e3daf2219fef49ed6b3129d8e6faceda5b5039
hellyaxs <eliasvitor.dev@gmail.com>
2024-11-21 17:07:07 -0300

    novo layout do projeto

--- src/app/share/models/campo-in.ts
+++
@@ -1,4 +0,0 @@
-export interface CampoIn {
-    tipo:string;
-    valor:any;
-}
--- src/app/share/models/params-config.ts
+++
@@ -1,8 +0,0 @@
-import { CampoIn } from "./campo-in";
-
-export interface ParamsConfig {
-    paginas: number;
-    limite: number;
-    pesquisaT?:string;
-    campoAdd?: CampoIn;
-}
